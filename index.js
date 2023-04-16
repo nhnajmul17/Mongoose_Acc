31a1a70266ae762da65d1241bc2257e7091aed1f
nhnajmul17 <nhnajmul17@gmail.com>
2023-04-16 23:48:56 +0600

    index.js final

--- index.js
+++ index.js
@@ -5,12 +5,9 @@ const { red } = require("colors")
 
 const Port = process.env.PORT || 5000
 
-
-
 mongoose.connect(`${process.env.DB_URL}`).then(() => {
     console.log("Mongodb Server connected".blue.bold)
 })
-
 
 app.listen(Port, () => {
     console.log(`Listening Tour Server at ${Port}`.yellow.bold)
